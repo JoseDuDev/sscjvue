e788133e6611ac9984b23c743405aa847aa0f0e8
unknown <josedudev@gmail.com>
2020-07-06 17:07:51 -0300

    Novo layout

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
-import Vue from 'vue'
+/*!
 
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
+ =========================================================
+ * Vue Now UI Kit - v1.1.0
+ =========================================================
 
-import App from './App.vue'
-import router from './router'
+ * Product Page: https://www.creative-tim.com/product/now-ui-kit
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
 
-Vue.config.productionTip = false
+ * Designed by www.invisionapp.com Coded by www.creative-tim.com
 
-Vue.use(VueMaterial)
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+import Vue from 'vue';
+import App from './App.vue';
+// You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
+import router from './router';
+import NowUiKit from './plugins/now-ui-kit';
+
+Vue.config.productionTip = false;
+
+Vue.use(NowUiKit);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
